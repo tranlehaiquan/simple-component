5c919d523adda05293aec9b2a75ad0e7375d94ed
tranlehaiquan <tranlehaiquan@gmail.com>
2019-01-21 01:03:26 +0700

    auto fix file with eslint

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import classnames from 'classnames';
 
 export default class Button extends Component {
-  render() {
-    const { 
-      autoFocus, 
-      disabled, 
-      nativeType, 
-      renderButton,
-      type,
-      loading,
-      ...restProps
-    } = this.props;
+	render() {
+		const { 
+			autoFocus, 
+			disabled, 
+			nativeType, 
+			type,
+			children,
+			...restProps
+		} = this.props;
 
-    const btnTypeClassname = type ? `sp-button--${type}` : '';
-    const btnClassnames = classnames('sp-button', btnTypeClassname, {
-      'sp-button--disabled': disabled
-    });
+		const btnTypeClassname = type ? `sp-button--${type}` : '';
+		const btnClassnames = classnames('sp-button', btnTypeClassname, {
+			'sp-button--disabled': disabled
+		});
 
-    return (
-      <button 
-        autoFocus={autoFocus}
-        disabled={disabled}
-        type={nativeType}
-        className={btnClassnames}
-        {...restProps}
-      >
-        { this.props.children }
-      </button>
-    )
-  }
+		return (
+			<button
+				autoFocus={autoFocus}
+				disabled={disabled}
+				type={nativeType}
+				className={btnClassnames}
+				{...restProps}
+			>
+				{ children }
+			</button>
+		);
+	}
 }
 
 Button.propTypes = {
-  autoFocus: propTypes.bool,
-  disabled: propTypes.bool,
-  type: propTypes.string,
-  nativeType: propTypes.oneOf(['button', 'submit', 'reset']),
-  // icon: propTypes.string,
-  // loading: propTypes.bool
-}
+	autoFocus: propTypes.bool,
+	disabled: propTypes.bool,
+	type: propTypes.string,
+	nativeType: propTypes.oneOf(['button', 'submit', 'reset']),
+	children: propTypes.node
+	// icon: propTypes.string,
+	// loading: propTypes.bool
+};
 
 Button.defaultProps = {
-  autoFocus: false,
-  disabled: false,
-  type: '',
-  nativeType: 'button',
-  // icon: '',
-  // loading: false
-}
+	autoFocus: false,
+	disabled: false,
+	type: '',
+	nativeType: 'button',
+	children: null  
+	// icon: '',
+	// loading: false
+};
