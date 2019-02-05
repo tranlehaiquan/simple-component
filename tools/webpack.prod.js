95f3eda6745f2aef3311a37cefdf9536e67f2239
tranlehaiquan <tranlehaiquan@gmail.com>
2019-02-06 01:17:27 +0700

    update script for publish

--- tools/webpack.prod.js
+++ tools/webpack.prod.js
@@ -7,31 +7,36 @@ const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const config = {
-  entry: path.resolve(__dirname, '../src/build.js'),
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    libraryTarget: 'commonjs2',
-    filename: 'index.js'
-  },
+module.exports = function (env, agr) {
+  const plugins = [
+    new CleanWebpackPlugin("dist", {
+      root: path.resolve(__dirname, '..')
+    })
+  ];
 
-  module: {
-    rules: [
-      {
+  if (!env.isPublish) {
+    isPublish.push(new BundleAnalyzerPlugin());
+  }
+
+  return {
+    entry: path.resolve(__dirname, '../src/build.js'),
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      libraryTarget: 'commonjs2',
+      filename: 'index.js'
+    },
+
+    module: {
+      rules: [{
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components|build)/,
         use: {
-          loader: "babel-loader", 
+          loader: "babel-loader",
           options: require('../babelrc.js')('production')
         }
-      }
-    ]
-  },
-  // finally we pass it an array of our plugins
-  plugins: [
-    new CleanWebpackPlugin("dist", { root: path.resolve(__dirname, '..') }),
-    new BundleAnalyzerPlugin(),
-  ]
+      }]
+    },
+    // finally we pass it an array of our plugins
+    plugins
+  }
 };
-
-module.exports = config;
