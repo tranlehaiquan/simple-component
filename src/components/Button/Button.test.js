650aac8696fa541ef746059b594f2893b8da6bb3
tranlehaiquan <tranlehaiquan@gmail.com>
2019-02-23 20:38:57 +0700

    Update test setup

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+import renderer from 'react-test-renderer';
+
+test('renderButton', () => {
+  const component = renderer.create(
+    <Button>ABC</Button>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
