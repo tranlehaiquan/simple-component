1cdf79f7b6b596b47764f5ddc9c977681387b8f2
tranlehaiquan <tranlehaiquan@gmail.com>
2019-05-10 15:01:25 +0700

    fix uppercase folder

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { default as Button } from './components/Button';
 export { default as Checkbox, CheckboxGroup } from './components/Checkbox';
 export { default as Radio, RadioGroup } from './components/Radio';
 export { default as Toast } from './components/Message';
-export { default as DatePicker, Calender, YearPanel, MonthPanel, DatePanel } from './components/Datepicker';
+export { default as DatePicker, Calender, YearPanel, MonthPanel, DatePanel } from './components/DatePicker';
